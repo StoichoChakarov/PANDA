9a86cf0553a334b1a0d0b8b8e9edf8f0592c9bfd
StoichoChakarov <stoicho.chakarov@gmail.com>
2022-10-26 20:44:12 +0300

    Add project files.

---
+++ Panda.Data/PandaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Panda.Models;
+using System;
+
+namespace Panda.Data
+{
+    public class PandaDBContext : IdentityDbContext<PandaUser, PandaUserRole, string>
+    {
+        public PandaDBContext(DbContextOptions<PandaDBContext> options)
+            : base(options) { }
+    }
+}
---
+++ Panda.Models/PandaUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda.Models
+{
+    public class PandaUser : IdentityUser
+    {
+        public ICollection<Package> Packages { get; set; }
+        public ICollection<Receipt> Receipts { get; set; }
+
+        public PandaUser()
+        {
+            this.Packages = new List<Package>();
+            this.Receipts = new List<Receipt>();
+        }
+    }
+}
